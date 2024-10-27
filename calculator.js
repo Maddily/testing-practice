b7aae51dbbe9e9d5bc9688ad9f9359320d3b250a
Maddie <115709272+Maddily@users.noreply.github.com>
2024-10-27 15:12:46 +0300

    Write calculator objects with methods
    The object contains methods to add, subtract,
    divide and multiply two given numbers.

---
+++ calculator.js
@@ -0,0 +1,16 @@
+const calculator = {
+  add(n1, n2) {
+    return (n1 * 1) + (n2 * 1);
+  },
+  subtract(n1, n2) {
+    return (n1 * 1) - (n2 * 1);
+  },
+  divide(n1, n2) {
+    return (n1 * 1) / (n2 * 1);
+  },
+  multiply(n1, n2) {
+    return (n1 * 1) * (n2 * 1);
+  }
+};
+
+export default calculator;
